d3b24671d506b2df636130a0a5c8cc2349886cca
LEONALDO JOSE NATHANAEL PASARIBU <leonaldo.pasaribu@bankmandiri.co.id>
2026-05-05 08:35:53 +0700

    style: change toast position from bottom to top

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
+      <Sonner position="top-center" />
       <BrowserRouter>
         <AuthProvider>
           <Routes>
