3ca0178c2307b9c1aae1741273fe0f13113d8f20
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2026-07-13 10:49:39 +0700

    feat: add Vercel Analytics integration to track user interactions

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
+
 import App from './App.tsx';
 import './index.css';
 import { setupPWA } from './pwa';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
 
 setupPWA();
